2cd33c2edc165a630ee58b74d860101dcee3f70e
vishal kumar <vishalkumar437@gmail.com>
2023-08-26 13:05:30 +0530

    added sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,60 @@
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import SchoolIcon from "@mui/icons-material/School";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  colors,
+} from "@mui/material";
+import Link from "next/link";
+export default function Sidebar() {
+  return (
+    <Box sx={{ borderRightWidth: "2px" }}>
+      <Box sx={{ height: "100%", padding: "4px" }}>
+        <Box sx={{ fontWeight: "700" }}>
+          <List disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <SpaceDashboardIcon sx={{color:"blue"}}/>
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+            <ListItemButton>
+
+                <ListItemIcon sx={{color:"blue"}}>
+                  <SchoolIcon />
+                </ListItemIcon>
+                <ListItemText primary="My Classes" />
+             
+            </ListItemButton>
+            <ListItemButton>
+                <ListItemIcon sx={{color:"blue"}}>
+                  <MenuBookIcon />
+                </ListItemIcon>
+
+                <ListItemText primary="Ebook" />
+            </ListItemButton>
+            <ListItemButton className="rounded-sm">
+                <ListItemIcon sx={{color:"blue"}}>
+                <AccountBalanceWalletIcon />
+                </ListItemIcon>
+                
+                <ListItemText primary="Transactions"/>
+            </ListItemButton>
+            <ListItemButton className="rounded-sm">
+                <ListItemIcon sx={{color:"blue"}}>
+                <SettingsSuggestIcon />
+                </ListItemIcon>
+                <ListItemText primary="Settings"/>
+            </ListItemButton>
+          </List>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Box } from "@mui/material";
+import Sidebar from "./components/Sidebar";
 
 export default function RootLayout({
   children,
@@ -15,12 +16,16 @@ export default function RootLayout({
           <Box sx={{ height: "10vh" }}>
             <Header />
           </Box>
-
+          <Box sx={{display:"flex"}}>
+          <Box sx={{position:"sticky",top:0, height: "100vh" }}>
+            <Sidebar/>
+            </Box>
           <Box sx={{ height: "80vh", display: "flex" }}>{children}</Box>
-
+          </Box>
           <Box sx={{ height: "10vh" }}>
             <Footer />
           </Box>
+
         </Box>
       </body>
     </html>
