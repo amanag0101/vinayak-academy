6f133189f4e5271c616776530333558d28d8e190
Aman Agarwal <amanag0101@gmail.com>
2023-08-30 04:33:31 +0530

    showing the doubt-support component in full screen

--- app/components/student/doubt-support/DoubtSupport.tsx
+++ app/components/student/doubt-support/DoubtSupport.tsx
@@ -3,9 +3,9 @@
 import { Box } from "@mui/material";
 import { DoubtSidebar } from "./DoubtSidebar";
 import { useSelector } from "react-redux";
-import { Doubt } from "@/app/features/page/doubtsSlice";
 import { CreateDoubt } from "./CreateDoubt";
 import Chat from "../../chat/Chat";
+import { Doubt } from "@/app/features/page/doubtsSlice";
 
 export const DoubtSupport = () => {
   const selectedDoubt = useSelector(
@@ -13,14 +13,17 @@ export const DoubtSupport = () => {
   );
 
   return (
-    <Box sx={{ height: "100%", width: "90%" }}>
-      <Box sx={{ display: "flex", alignItems: "center", height: "100%" }}>
-        <Box sx={{ border: "1px solid #ccc" }}>
+    <Box sx={{ height: "100%", width: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          height: "100%",
+        }}
+      >
+        <Box sx={{ borderRight: "1px solid #ccc" }}>
           <DoubtSidebar />
         </Box>
-        <Box sx={{ height: "auto", width: "100%", border: "1px solid #ccc" }}>
-          {selectedDoubt ? <Chat /> : <CreateDoubt />}
-        </Box>
+        <Box sx={{ flex: 1 }}>{selectedDoubt ? <Chat /> : <CreateDoubt />}</Box>
       </Box>
     </Box>
   );
